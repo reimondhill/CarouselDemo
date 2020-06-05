f0bfed86517d7714ff088ea80f00a28b86cfcccc
Ramon Haro Marques <ramonharomarques@gmail.com>
2020-06-05 18:25:36 +0100

    Initial Commit

---
+++ CarouselDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarouselDemo
+//
+//  Created by Ramon Haro Marques on 05/06/2020.
+//  Copyright © 2020 Ramon Haro Marques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
